de3a7f263294eee8ce63290b4bef034fc31d7673
Travis Hammonds <travishammonds2@gmail.com>
2022-06-22 20:37:20 -0700

    refactored functions

--- battlefield.js
+++ battlefield.js
@@ -1,16 +1,23 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const path = require('path');
+// const app = express();
 
-app.use(express.static(__dirname + '/static'));
+// app.use(bodyParser.urlencoded({extended: true})); //allows app to use bodyParser. urlencoded gets data 
 
-app.use(express.static(__dirname + '/'));
+// app.use(express.static(__dirname + '/static'));
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+// app.use(express.static(__dirname + '/'));
 
-app.listen(8000, function () {
-    console.log('Listening on port 8000');
-});
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/index.html'));
+// });
+
+// app.post('/', function(req, res) {
+// })
+
+// app.listen(8000, function () {
+//     console.log('Listening on port 8000');
+// });
+
 
--- index.js
+++ index.js
@@ -1,25 +1,87 @@
 
-const yellowPiece = $(".yellow-piece");
+let selectedPegClicked = false;
+let moves = 0;
+let selectedPeg;
+let selectedPegColor;
+let selectedPegId;
+let openSpace = $(".open");
 
-const greenPiece = $(".green-piece");
+console.log(selectedPegClicked, moves, selectedPeg, selectedPegColor, 
+    selectedPegId, openSpace);
 
-const openSpace = $("#open-space");
-
-$("i").click(function () {
-    let selectedPeg = $(this);
-    let selectID = $(selectedPeg).attr('id');
-    let selectClass = $(selectedPeg).attr('class');
-    $('#open-space').click(function () {
-        let openSpace = $(this);
-        let openID = $(openSpace).attr('id');
-        let openClass = $(openSpace).attr('class');
-        $(openSpace).attr('id', selectID);
-        $(openSpace).attr('class', selectClass);
-        $(selectedPeg).attr('id', openID);
-        $(selectedPeg).attr('class', openClass);
-    }); 
+$(document).on('click', '.peg', function () {
+    selectedPeg = $(this);
+    selectedPegColor = $(this).attr("title");
+    selectedPegClicked = true;
+    selectedPegId = selectedPeg.attr("id");
 });
 
-// $('#g1').click(function () {
-//     $(this).css('color', 'red');
-// });
+$(document).on('click', '.open', function () {
+    if (selectedPegClicked === true) {
+        makeColoredSpace(openSpace, selectedPegColor);
+        addPegClass(openSpace);
+        removeOpenClass(openSpace)
+        makeOpen(selectedPeg);
+        resetVariables();
+    }
+});
+
+$(document).on('click', '#reset', function () {
+    resetBoard();
+});
+
+function makeOpen(peg) {
+    $(peg).removeClass("peg");
+    $(peg).addClass("open");
+    $(peg).attr("title", "");
+}
+
+function resetVariables() {
+    selectedPegClicked = false;
+    moves++;
+    selectedPeg = undefined;
+    selectedPegColor = undefined;
+    selectedPegId = undefined;
+    openSpace = $(".open");
+    console.log("new variables:");
+    console.log(selectedPegClicked, moves, selectedPeg, selectedPegColor, 
+    selectedPegId, openSpace);
+}
+
+function resetBoard() {
+    resetVariables();
+    for (i=1; i<=8; i++) {
+        var greenSpaceReset = $("#g"+i);
+        var color = 'green';
+        addPegClass(greenSpaceReset);
+        removeOpenClass(greenSpaceReset);
+        makeColoredSpace(greenSpaceReset, color);
+    }
+
+    for (i=1; i<=8; i++) {
+        var yellowSpaceReset = $("#y"+i);
+        var color = 'yellow';
+        addPegClass(yellowSpaceReset);
+        removeOpenClass(yellowSpaceReset);
+        makeColoredSpace(yellowSpaceReset, color);
+    }
+
+    makeOpen($("#open-space")); 
+}
+
+function addPegClass(peg) {
+    $(peg).addClass("peg");
+}
+
+function removeOpenClass(peg) {
+    $(peg).removeClass("open");
+}
+
+function makeColoredSpace(peg, color) {
+    $(peg).attr("title", color);
+
+}
+
+function increaseMoves() {
+    
+}
